b4a6c3f416c524c780bc5740374fc63e8f64ff0e
Alan Nguyen <23365399+anguyen120@users.noreply.github.com>
2021-11-07 01:08:31 -0500

    Implemented website layout (#2)
    - Removed daisyUI
    - Removed init stuff
    - Modify blue to match school blue
    - Added fonts

---
+++ components/MissionStatement.js
@@ -0,0 +1,98 @@
+export default function MissionStatement() {
+  return (
+    <section className="text-gray-600 body-font">
+      <div className="container px-5 py-24 mx-auto">
+        <div className="text-center mb-10">
+          <h1 className="sm:text-3xl text-2xl font-medium title-font text-gray-900 mb-4">
+            Raw Denim Heirloom Man Braid
+          </h1>
+          <p className="text-base leading-relaxed xl:w-2/4 lg:w-3/4 mx-auto text-gray-500s">
+            Blue bottle crucifix vinyl post-ironic four dollar toast vegan
+            taxidermy. Gastropub indxgo juice poutine, ramps microdosing banh mi
+            pug.
+          </p>
+        </div>
+          <div className="flex relative pb-20 sm:items-center md:w-2/3 mx-auto">
+            <div className="flex-grow md:pl-8 pl-6 flex sm:items-center items-start flex-col sm:flex-row">
+              <div className="flex-shrink-0 w-24 h-24 bg-blue-500 text-white rounded-full inline-flex items-center justify-center">
+                <svg
+                  fill="none"
+                  stroke="currentColor"
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  strokeWidth={2}
+                  className="w-12 h-12"
+                  viewBox="0 0 24 24"
+                >
+                  <path d="M22 12h-4l-3 9L9 3l-3 9H2" />
+                </svg>
+              </div>
+              <div className="flex-grow sm:pl-6 mt-6 sm:mt-0">
+                <h2 className="font-medium title-font text-gray-900 mb-1 text-xl">
+                  The Catalyzer
+                </h2>
+                <p className="leading-relaxed">
+                  VHS cornhole pop-up, try-hard 8-bit iceland helvetica. Kinfolk
+                  bespoke try-hard cliche palo santo offal.
+                </p>
+              </div>
+            </div>
+          </div>
+          <div className="flex relative pb-20 sm:items-center md:w-2/3 mx-auto">
+            <div className="flex-grow md:pl-8 pl-6 flex sm:items-center items-start flex-col sm:flex-row">
+              <div className="flex-shrink-0 w-24 h-24 bg-blue-500 text-white rounded-full inline-flex items-center justify-center">
+                <svg
+                  fill="none"
+                  stroke="currentColor"
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  strokeWidth={2}
+                  className="w-12 h-12"
+                  viewBox="0 0 24 24"
+                >
+                  <circle cx={12} cy={5} r={3} />
+                  <path d="M12 22V8M5 12H2a10 10 0 0020 0h-3" />
+                </svg>
+              </div>
+              <div className="flex-grow sm:pl-6 mt-6 sm:mt-0">
+                <h2 className="font-medium title-font text-gray-900 mb-1 text-xl">
+                  The 400 Blows
+                </h2>
+                <p className="leading-relaxed">
+                  VHS cornhole pop-up, try-hard 8-bit iceland helvetica. Kinfolk
+                  bespoke try-hard cliche palo santo offal.
+                </p>
+              </div>
+            </div>
+          </div>
+          <div className="flex relative pb-10 sm:items-center md:w-2/3 mx-auto">
+            <div className="flex-grow md:pl-8 pl-6 flex sm:items-center items-start flex-col sm:flex-row">
+              <div className="flex-shrink-0 w-24 h-24 bg-blue-500 text-white rounded-full inline-flex items-center justify-center">
+                <svg
+                  fill="none"
+                  stroke="currentColor"
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  strokeWidth={2}
+                  className="w-12 h-12"
+                  viewBox="0 0 24 24"
+                >
+                  <path d="M20 21v-2a4 4 0 00-4-4H8a4 4 0 00-4 4v2" />
+                  <circle cx={12} cy={7} r={4} />
+                </svg>
+              </div>
+              <div className="flex-grow sm:pl-6 mt-6 sm:mt-0">
+                <h2 className="font-medium title-font text-gray-900 mb-1 text-xl">
+                  Neptune
+                </h2>
+                <p className="leading-relaxed">
+                  VHS cornhole pop-up, try-hard 8-bit iceland helvetica. Kinfolk
+                  bespoke try-hard cliche palo santo offal.
+                </p>
+              </div>
+            </div>
+          </div>
+      </div>
+    </section>
+  );
+}
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,14 +1,33 @@
+const defaultTheme = require('tailwindcss/defaultTheme')
+
 module.exports = {
   mode: 'jit',
   purge: ['./pages/**/*.{js,ts,jsx,tsx}', './components/**/*.{js,ts,jsx,tsx}'],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        mono: ['"JetBrains Mono"', ...defaultTheme.fontFamily.mono],
+        sans: ['Oxygen', ...defaultTheme.fontFamily.sans],
+      },
+      colors: {
+        blue: {
+          "50": "#42a3e8",
+          "100": "#3899de",
+          "200": "#2e8fd4",
+          "300": "#2485ca",
+          "400": "#1a7bc0",
+          "500": "#1071b6",
+          "600": "#0667ac",
+          "700": "#005da2",
+          "800": "#005398",
+          "900": "#00498e"
+        }
+      },
+    },
   },
   variants: {
     extend: {},
   },
-  plugins: [
-    require('daisyui'),
-  ],
+  plugins: [],
 }
